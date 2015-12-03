ad42a258f69fdd25e86a16c968cabe67d753da91
chendamon <wangchendamon@gmail.com>
2015-12-03 16:50:18 +0800

    ogg test, jar ?

---
+++ src/oggTest.java
@@ -0,0 +1,11 @@
+
+public class oggTest 
+{
+	public static void main(String[] args)
+	{
+		metaNotePlay play = new metaNotePlay();
+		System.out.println("now listen...");
+		play.play("C:/Users/biront/Desktop/∏÷«Ÿ“ÙΩ◊“Ù–ß£®88“Ù£©/raw/a0.ogg");
+	}
+
+}
